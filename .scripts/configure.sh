4d0e35feeedd86c8e766cb6cbf03ef294b4fa329
Tim Schlottmann <timsc@ziva.2137.eu>
2020-05-11 14:54:19 +0200

    Added IPv6 and added binding to any interface

--- .scripts/configure.sh
+++ .scripts/configure.sh
@@ -16,7 +16,8 @@ configure() {
     echo "DHCP_ENABLED=1" >> settings.conf
     echo "FREERADIUS_ENABLED=1" >> settings.conf
     printf "\n# Docker settings\n" >> settings.conf
-    echo "BRIDGE_NET=\"10.201.0\"" >> settings.conf
+    echo "NET_IPv4_SUBNET=\"10.201.0\"" >> settings.conf
+    echo "NET_IPv6_SUBNET=\"fd00::/80\"" >> settings.conf
     printf "\n# DNS settings\n" >> settings.conf
     echo "DNS_LISTENING_ADDRESS=\"$IP_ADDRESS\"" >> settings.conf
     echo "DNS_LISTENING_PORT=53" >> settings.conf
@@ -41,14 +42,33 @@ updateDockerCompose() {
   sed -i "s/%%DNS_ENABLED%%/${DNS_ENABLED}/g" ./docker-compose.yml
   sed -i "s/%%DHCP_ENABLED%%/${DHCP_ENABLED}/g" ./docker-compose.yml
   sed -i "s/%%FREERADIUS_ENABLED%%/${FREERADIUS_ENABLED}/g" ./docker-compose.yml
-  sed -i "s/%%BRIDGE_NET%%/${BRIDGE_NET}/g" ./docker-compose.yml
-  sed -i "s/%%DNS_LISTENING_ADDRESS%%/${DNS_LISTENING_ADDRESS}/g" ./docker-compose.yml
-  sed -i "s/%%DNS_LISTENING_PORT%%/${DNS_LISTENING_PORT}/g" ./docker-compose.yml
+  sed -i "s/%%NET_IPv4_SUBNET%%/${NET_IPv4_SUBNET}/g" ./docker-compose.yml
+  sed -i "s/%%NET_IPv6_SUBNET%%/${NET_IPv6_SUBNET}/g" ./docker-compose.yml
+  sed -i "s/%%NET_IPv6_SUFFIX%%/${NET_IPv6_SUFFIX}/g" ./docker-compose.yml
+  if [ !  -z  $DNS_LISTENING_ADDRESS ]; then
+    echo "1a $DNS_LISTENING_ADDRESS"
+    sed -i "s/%%DNS_LISTENING_ADDRESS%%/${DNS_LISTENING_ADDRESS}:/g" ./docker-compose.yml
+    sed -i "s/%%DNS_LISTENING_PORT%%/${DNS_LISTENING_PORT}/g" ./docker-compose.yml
+  else
+    echo "1b $DNS_LISTENING_ADDRESS"
+    sed -i "s/%%DNS_LISTENING_ADDRESS%%/${DNS_LISTENING_ADDRESS}/g" ./docker-compose.yml
+    sed -i "s/%%DNS_LISTENING_PORT%%/${DNS_LISTENING_PORT}/g" ./docker-compose.yml
+  fi
   sed -i "s/%%DNS_BRIDGE_HOST%%/${DNS_BRIDGE_HOST}/g" ./docker-compose.yml
-  sed -i "s/%%DHCP_LISTENING_ADDRESS%%/${DHCP_LISTENING_ADDRESS}/g" ./docker-compose.yml
-  sed -i "s/%%DHCP_LISTENING_PORT%%/${DHCP_LISTENING_PORT}/g" ./docker-compose.yml
+  if [ !  -z  $DHCP_LISTENING_ADDRESS ]; then
+    sed -i "s/%%DHCP_LISTENING_ADDRESS%%/${DHCP_LISTENING_ADDRESS}:/g" ./docker-compose.yml
+    sed -i "s/%%DHCP_LISTENING_PORT%%/${DHCP_LISTENING_PORT}/g" ./docker-compose.yml
+  else
+    sed -i "s/%%DHCP_LISTENING_ADDRESS%%/${DHCP_LISTENING_ADDRESS}/g" ./docker-compose.yml
+    sed -i "s/%%DHCP_LISTENING_PORT%%/${DHCP_LISTENING_PORT}/g" ./docker-compose.yml
+  fi
   sed -i "s/%%DHCP_BRIDGE_HOST%%/${DHCP_BRIDGE_HOST}/g" ./docker-compose.yml
-  sed -i "s/%%FREERADIUS_LISTENING_ADDRESS%%/${FREERADIUS_LISTENING_ADDRESS}/g" ./docker-compose.yml
-  sed -i "s/%%FREERADIUS_LISTENING_PORT%%/${FREERADIUS_LISTENING_PORT}/g" ./docker-compose.yml
+  if [ !  -z  $FREERADIUS_LISTENING_ADDRESS ]; then
+    sed -i "s/%%FREERADIUS_LISTENING_ADDRESS%%/${FREERADIUS_LISTENING_ADDRESS}:/g" ./docker-compose.yml
+    sed -i "s/%%FREERADIUS_LISTENING_PORT%%/${FREERADIUS_LISTENING_PORT}/g" ./docker-compose.yml
+  else
+    sed -i "s/%%FREERADIUS_LISTENING_ADDRESS%%/${FREERADIUS_LISTENING_ADDRESS}/g" ./docker-compose.yml
+    sed -i "s/%%FREERADIUS_LISTENING_PORT%%/${FREERADIUS_LISTENING_PORT}/g" ./docker-compose.yml
+  fi
   sed -i "s/%%FREERADIUS_BRIDGE_HOST%%/${FREERADIUS_BRIDGE_HOST}/g" ./docker-compose.yml
 }
