59913e74676c8869a0c8ccd2fecf57cdc6d66d9d
Tim Schlottmann <tschlottmann@gmail.com>
2020-06-03 20:21:06 +0200

    Permission fix

--- container/dns/run.sh
+++ container/dns/run.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 if [ $ENABLED -eq 1 ]; then
+  #Set timezone
   if [ -f /etc/localtime ]; then
     rm /etc/localtime
   fi
   ln -s /usr/share/zoneinfo/${TIMEZONE:-"UTC"} /etc/localtime
   echo ${TIMEZONE:-"UTC"} > /etc/timezone
 
-  named -f
+  #Permission fix
+  chown -R 202 /etc/bind
+  find /etc/bind -type d -exec chmod 775 {} +
+  find /etc/bind -type f -exec chmod 664 {} +
+
+  named -f -u named
 else
   sleep 365d
 fi
--- container/freeradius/run.sh
+++ container/freeradius/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 if [ $ENABLED -eq 1 ]; then
+  #Set timezone
   if [ -f /etc/localtime ]; then
     rm /etc/localtime
   fi
   ln -s /usr/share/zoneinfo/${TIMEZONE:-"UTC"} /etc/localtime
   echo ${TIMEZONE:-"UTC"} > /etc/timezone
+
+  #Permission fix
+  chown -R 201 /etc/raddb
+  find /etc/raddb -type d -exec chmod 755 {} +
+  find /etc/raddb -type f -exec chmod 644 {} +
 
   radiusd -f -l stdout
 else
