c8ac2f71d68899a4d7943ef1df50affca75395d3
TheTimmoth <github@timsc.de>
2021-03-17 11:06:53 +0100

    Change BIND9 version to 9.16.11 and e-mail address

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -2,16 +2,17 @@
 
 run() {
   #Permission fix
-  chown -R :202 /etc/bind
+  chown -R 202:202 /etc/bind
   find /etc/bind -type d -exec chmod g+s {} +
   find /etc/bind -type d -exec chmod 775 {} +
   find /etc/bind -type f -exec chmod 664 {} +
   find /etc/bind -type f -name "*.sh" -exec chmod 774 {} +
+  chmod 600 /etc/bind/keys/*.private
 
   named -f -u bind
 }
 
 install() {
   cp -r /etc/bind/* /template
-  chown -R :202 /template
+  chown -R 202:202 /template
 }
