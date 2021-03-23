4eff9628825d7a6fa3baafa165bf8a4065ff0662
TheTimmoth <github@timsc.de>
2021-03-23 17:14:40 +0100

    Move default settings to seperate file

--- install.sh
+++ install.sh
@@ -4,38 +4,17 @@
 
 configure() {
 
-  if [ -f /etc/timezone ]; then
+  if [ -f /etc/timezone ]
+  then
     TIMEZONE=$(cat /etc/timezone)
-  elif [ -f /etc/localtime ]; then
+  elif [ -f /etc/localtime ]
+  then
     TIMEZONE=$(ls -l /etc/localtime | sed -n "s#^.*zoneinfo/\(.*$\)#\1#p")
   fi
 
-  if [ ! -f settings.conf ]; then
-    printf "No settings file detectes. Creating new one...\n"
-    printf "#########################################################\n" > settings.conf
-    printf "# server_services configuration file\n" >> settings.conf
-    printf "#########################################################\n" >> settings.conf
-    printf "\n# General settings\n" >> settings.conf
-    echo "DNS_ENABLED=0" >> settings.conf
-    echo "DHCP_ENABLED=0" >> settings.conf
-    echo "FREERADIUS_ENABLED=0" >> settings.conf
-    echo "" >> settings.conf
-    echo "TIMEZONE=${TIMEZONE:-"UTC"}" >> settings.conf
-    echo "" >> settings.conf
-    echo "BUILD_DISABLED_CONTAINERS=0" >> settings.conf
-    printf "\n# Docker settings\n" >> settings.conf
-    echo "NET_IPv4_PREFIX=10.201.0" >> settings.conf
-    echo "NET_IPv6_PREFIX=fd1a:2b17:1d42:cddd:" >> settings.conf
-    echo "NET_IPv6_NETMASK=80" >> settings.conf
-    printf "\n# DNS settings\n" >> settings.conf
-    echo "DNS_BINDING=53" >> settings.conf
-    echo "DNS_BRIDGE_HOST=2" >> settings.conf
-    printf "\n# DHCP settings\n" >> settings.conf
-    echo "DHCP_BINDING=67" >> settings.conf
-    echo "DHCP_BRIDGE_HOST=3" >> settings.conf
-    printf "\n# FREERADIUS settings\n" >> settings.conf
-    echo "FREERADIUS_BINDING=1812" >> settings.conf
-    echo "FREERADIUS_BRIDGE_HOST=4" >> settings.conf
+  if [ ! -f settings.conf ]
+  then
+    cp etc/settings.template ./settings.conf
   fi
   source settings.conf
 }
